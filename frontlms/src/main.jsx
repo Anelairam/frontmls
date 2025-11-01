e48cdab8de96ab13fed13e3964eb090a421323f6
Mel <m.livathinopoulou@hotmail.com>
2025-11-01 14:49:05 +0100

    adding auth0

--- frontlms/src/main.jsx
+++ frontlms/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
 import App from './App.jsx'
 
+const clientId = import.meta.env.VITE_AUTH_CLIENT_ID;
+const domain = import.meta.env.VITE_AUTH_DOMAIN;
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+   <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>
   </StrictMode>,
 )
