8249e36291b4eb8c6059020e904cea1a04827c97
Mel <m.livathinopoulou@hotmail.com>
2025-11-02 12:16:03 +0100

    tailwind

--- frontlms/src/App.jsx
+++ frontlms/src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
 import LoginButton from './components/organisms/login.jsx'
 import LogoutButton from './components/organisms/logout.jsx'
+import { Profile } from './components/templates/Profile.jsx'
 
 
 const App = () =>{
   return(
     <div>
-      <h1>Learning Management System</h1>
+      <h1>You said Greek</h1>
       <LoginButton />
       <LogoutButton/>
+      <Profile />
     </div>
   )
 }
